9d1d31962bf1147ec008f85a4186c930e9aaffa6
Niko Craft <nermion@gmail.com>
2019-12-01 22:50:47 +0100

    corrected powered by link in footer

--- src/views/includes/footer.blade.php
+++ src/views/includes/footer.blade.php
@@ -1,4 +1,4 @@
 <footer>
     <div class="footer-copyright">{{ get_theme_setting('footer.general.copyright') }} {{ now()->year }}</div>
-    <div class="footer-poweredby"><a href="https://www.laraone.com">Powered by Laraone</a></div>
+    <div class="footer-poweredby">Powered by <a href="https://www.laraone.com">Laraone</a></div>
 </footer>
